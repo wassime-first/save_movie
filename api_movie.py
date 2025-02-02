5aaaee2d2a6650411fe5e882e09de88b86c9d8b0
wassime <wassimetestmail@gmail.com>
2025-02-02 16:37:36 +0100

    first commit app v1.00

---
+++ api_movie.py
@@ -0,0 +1,157 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+IMG = os.getenv("IMG")
+API_KEY = os.getenv("API_KEY")
+API_LONG_KEY = os.getenv("API_LONG_KEY")
+api_key = API_KEY
+api_long_key = API_LONG_KEY
+IMG_LINK = os.getenv("IMG_LINK")
+url = f"https://api.themoviedb.org/3/authentication"
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {api_long_key}",
+}
+
+
+def search_tv(title: str):
+    url_searsh = "https://api.themoviedb.org/3/search/tv"
+    params = {
+        "query": title,
+        # "api_key": api_key,
+        "language": "en-US",
+        "page": 1,
+        "include_adult": True,
+    }
+
+    response_search = requests.get(url_searsh, headers=headers, params=params)
+    data = response_search.json()
+    tv_list = []
+    for title in data["results"]:
+        d = (f"{title['name']} -  {title['first_air_date']}", f"{IMG_LINK}{title['backdrop_path']}", title["id"])
+        tv_list.append(d)
+    return tv_list
+
+
+def tv_data(id):
+    url = f"https://api.themoviedb.org/3/tv/{id}"
+    response_movie = requests.get(url, headers=headers)
+    data = response_movie.json()
+    title = data["original_name"]
+    release_date = data["last_air_date"]
+    img_url = f'{IMG_LINK}{data["poster_path"]}'
+    return (title, release_date, img_url)
+
+
+def search_movies(title: str):
+    url_searsh = "https://api.themoviedb.org/3/search/movie"
+    params = {
+        "query": title,
+        # "api_key": api_key,
+        "language": "en-US",
+        "page": 1,
+        "include_adult": True,
+    }
+
+    response_search = requests.get(url_searsh, headers=headers, params=params)
+    data = response_search.json()
+    movies_list = []
+    movie_id_list = []
+    for title in data["results"]:
+        movies_list.append((f"{title['title']} -  {title['release_date']}", f"{IMG_LINK}{title['backdrop_path']}"))
+        movie_id_list.append(title["id"])
+    return (movies_list, movie_id_list)
+
+
+def movie_data(id):
+    url_movie = f"https://api.themoviedb.org/3/movie/{id}"
+    response_movie = requests.get(url_movie, headers=headers)
+    data = response_movie.json()
+    title = data["original_title"]
+    release_date = data["release_date"]
+    overview = data["overview"]
+    img_url = f'{IMG_LINK}{data["backdrop_path"]}'
+    return (title, release_date, overview, img_url)
+
+
+def search_all(title):
+    url_all = "https://api.themoviedb.org/3/search/multi"
+    params = {
+        "query": title,
+        # "api_key": api_key,
+        "language": "en-US",
+        "page": 1,
+        "include_adult": True,
+    }
+    response_all = requests.get(url=url_all, headers=headers, params=params)
+    data = response_all.json()
+    all_results = []
+    for i in data["results"]:
+        try:
+            if i["known_for"][0]["poster_path"]:
+                add = (i["id"], i["name"], f'{IMG_LINK}{i["known_for"][0]["poster_path"]}', "pp")
+                all_results.append(add)
+            else:
+                add = (i["id"], i["name"], f'{IMG_LINK}{i["known_for"][0]["backdrop_path"]}', "pp")
+                all_results.append(add)
+        except IndexError:
+            add = (i["id"], i["name"], f'{IMG}', "pp")
+            all_results.append(add)
+        except KeyError:
+            try:
+                add = (i["id"], i["title"], f"{IMG_LINK}{i['backdrop_path']}", i["media_type"])
+                all_results.append(add)
+            except KeyError:
+                add = (i["id"], i["name"], f"{IMG_LINK}{i['backdrop_path']}", i["media_type"])
+                all_results.append(add)
+    return all_results
+
+
+def search_ppl(title, page):
+    url_searsh = "https://api.themoviedb.org/3/search/person"
+    params = {
+        "query": title,
+        # "api_key": api_key,
+        "language": "en-US",
+        "page": page,
+        "include_adult": True,
+    }
+
+    response_search = requests.get(url_searsh, headers=headers, params=params)
+    data = response_search.json()
+    actor_list = []
+    for title in data["results"]:
+        d = (f"{title['name']}", f"{IMG_LINK}{title['profile_path']}", title["id"])
+        actor_list.append(d)
+    return actor_list
+
+
+def ppl_data(id):
+    url_movie = f"https://api.themoviedb.org/3/person/{id}"
+    response_movie = requests.get(url_movie, headers=headers)
+    data = response_movie.json()
+    name = data["name"]
+    img_url = f'{IMG_LINK}{data["profile_path"]}'
+    return (name, img_url)
+
+
+def discover_movies(page):
+    url = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page={page}&sort_by=popularity.desc"
+    response = requests.get(url=url, headers=headers)
+    data = response.json()
+    dicovery_list = []
+    for i in data["results"]:
+        title = i["title"]
+        id = i["id"]
+        rating = i["vote_average"]
+        img = f'{IMG_LINK}{i["backdrop_path"]}'
+        d = (title, img, id, rating)
+        dicovery_list.append(d)
+    return dicovery_list
+
+# print(tv_data(94605))
+
+# print(movie_data("109445"))
+# print(discover_movies(1))
---
+++ main.py
@@ -0,0 +1,244 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField, PasswordField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import api_movie
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_URI = os.getenv("DB_URI")
+FLASK_KEY = os.getenv("FLASK_KEY")
+IMG = os.getenv("IMG")
+NIMG = "https://image.tmdb.org/t/p/originalNone"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = f'{FLASK_KEY}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{DB_URI}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+Bootstrap(app)
+login_manager = LoginManager(app)
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+db = SQLAlchemy(app)
+db.echo = True
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users_saves"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(1000), nullable=False)
+    models = db.relationship("Model", backref="user", cascade="all, delete, save-update")
+    tvs = db.relationship("Tv", backref="user", cascade="all, delete, save-update")
+    actors = db.relationship("Actor", backref="user", cascade="all, delete, save-update")
+    movies = db.relationship("Movie", backref="user", cascade="all, delete, save-update")
+
+
+class Model(UserMixin, db.Model):
+    __tablename__ = "models"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users_saves.id", ondelete="CASCADE", onupdate="CASCADE"),
+                        nullable=False)
+
+
+class Movie(UserMixin, db.Model):
+    __tablename__ = "movies_saves"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users_saves.id", ondelete="CASCADE", onupdate="CASCADE"),
+                        nullable=False)
+
+
+class Tv(UserMixin, db.Model):
+    __tablename__ = "tvs"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users_saves.id", ondelete="CASCADE", onupdate="CASCADE"),
+                        nullable=False)
+
+
+class Actor(UserMixin, db.Model):
+    __tablename__ = "actors"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users_saves.id", ondelete="CASCADE", onupdate="CASCADE"),
+                        nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class MovieForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Search")
+
+
+@app.route("/")
+@login_required
+def main():
+    return redirect(url_for("discover", page=1))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect("/")
+        if not user:
+            email = form.email.data
+            password = form.password.data
+            new_user = User(email=email, password=generate_password_hash(password, salt_length=8))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect("/")
+    return render_template("login.html", form=form)
+
+
+@app.route("/add", methods=["POST", "GET"])
+@login_required
+def add():
+    """ Searching methods """
+    if "s" in request.args:
+        if request.args.get("s") == "movie":
+            if request.method == "POST":
+                movie_name = request.form.get("movie")
+                movie_data = api_movie.search_movies(movie_name)
+                return render_template("search.html", s="movie", movie=movie_data, nimg=NIMG, img=IMG)
+            return render_template("search.html", s="movie")
+        elif request.args.get("s") == "series":
+            if request.method == "POST":
+                tv_name = request.form.get("movie")
+                tv_data = api_movie.search_tv(tv_name)
+                return render_template("search.html", s="series", tv=tv_data, nimg=NIMG, img=IMG)
+            return render_template("search.html", s="series")
+        elif request.args.get("s") == "actors":
+            if request.method == "POST":
+                ppl_name = request.form.get("movie")
+                if request.args.get("page"):
+                    page = int(request.args.get("page"))
+                    ppl_data = api_movie.search_ppl(ppl_name, int(request.args.get("page")))
+                    return render_template("search.html", page=page, name=ppl_name, s="actors", actor=ppl_data,
+                                           nimg=NIMG, img=IMG)
+                else:
+                    ppl_data = api_movie.search_ppl(ppl_name, 1)
+                    return render_template("search.html", page=1, name=ppl_name, s="actors", actor=ppl_data, nimg=NIMG,
+                                           img=IMG)
+            elif "name" in request.args and "page" in request.args:
+                page = int(request.args.get("page"))
+                ppl_name = request.args.get("name")
+                ppl_data = api_movie.search_ppl(ppl_name, page)
+                return render_template("search.html", name=ppl_name, s="actors", actor=ppl_data, nimg=NIMG, img=IMG)
+            return render_template("search.html", s="actors")
+
+
+
+    else:
+        if request.method == "POST":
+            search = request.form.get("movie")
+            search_data = api_movie.search_all(search)
+            return render_template("search.html", all=search_data, img=IMG, nimg=NIMG)
+
+    """ adding methods """
+    if "movie_id" in request.args:
+        if request.args.get("media") == "movie":
+            movie_id = request.args.get("movie_id")
+            movie_data = api_movie.movie_data(movie_id)
+            new_movie = Movie(name=movie_data[0], url=movie_data[3], user_id=current_user.id)
+            db.session.add(new_movie)
+            db.session.commit()
+            return redirect("/")
+        elif request.args.get("media") == "tv":
+            movie_id = request.args.get("movie_id")
+            tv_data = api_movie.tv_data(movie_id)
+            new_movie = Tv(name=tv_data[0], url=tv_data[2], user_id=current_user.id)
+            db.session.add(new_movie)
+            db.session.commit()
+            return redirect("/")
+        elif request.args.get("media") == "pp":
+            movie_id = request.args.get("movie_id")
+            movie_data = api_movie.ppl_data(movie_id)
+            new_movie = Actor(name=movie_data[0], url=movie_data[1], user_id=current_user.id)
+            db.session.add(new_movie)
+            db.session.commit()
+            return redirect("/")
+    return render_template("search.html", nimg=NIMG, img=IMG)
+
+
+@app.route("/discover/<page>")
+@login_required
+def discover(page):
+    if page is None:
+        page = 1
+    data = api_movie.discover_movies(page)
+    return render_template("index.html", page=page, movies=data, nimg=NIMG, img=IMG)
+
+
+@app.route("/movies")
+@login_required
+def movie():
+    all_movies = db.session.query(Movie).filter(Movie.user_id == current_user.id).all()
+    if "movie_id" in request.args:
+        deleted_movie = db.session.query(Movie).filter(Movie.id == request.args.get("movie_id")).first()
+        db.session.delete(deleted_movie)
+        db.session.commit()
+        return redirect("/movies")
+    return render_template("movies.html", movies=all_movies)
+
+
+@app.route("/tv")
+@login_required
+def tv():
+    all_tv = db.session.query(Tv).filter(Tv.user_id == current_user.id).all()
+    if "movie_id" in request.args:
+        deleted_movie = db.session.query(Tv).filter(Tv.id == request.args.get("movie_id")).first()
+        db.session.delete(deleted_movie)
+        db.session.commit()
+        return redirect("/tv")
+    return render_template("movies.html", tv=all_tv)
+
+
+@app.route("/actors")
+@login_required
+def actor():
+    all_actors = db.session.query(Actor).filter(Actor.user_id == current_user.id).all()
+    if "movie_id" in request.args:
+        deleted_movie = db.session.query(Actor).filter(Actor.id == request.args.get("movie_id")).first()
+        db.session.delete(deleted_movie)
+        db.session.commit()
+        return redirect("/actors")
+    return render_template("movies.html", actor=all_actors)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect("login")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run()
